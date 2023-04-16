60693c2762b3044a4a7a600adebc08e19f9bae1d
peeranat-dan <ppeeranat.d@gmail.com>
2023-04-16 10:29:06 +0700

    feat: add react query to apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import ThemeProvider from './providers/ThemeProvider';
 import AppRouter from './router';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+
 function App() {
-  return <AppRouter />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AppRouter />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
