c4fb3462632d8a0037d9d436ca04aa2a63e28016
peeranat-dan <ppeeranat.d@gmail.com>
2023-05-14 23:56:52 +0700

    🎨 add Button variants to stories

--- src/components/base/button/Button.stories.ts
+++ src/components/base/button/Button.stories.ts
@@ -14,6 +14,28 @@ type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
   args: {
+    variant: 'primary',
+    children: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+};
+
+export const Outlined: Story = {
+  args: {
+    variant: 'outlined',
+    children: 'Button',
+  },
+};
+
+export const Text: Story = {
+  args: {
+    variant: 'text',
     children: 'Button',
   },
 };
