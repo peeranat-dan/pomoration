d0528c5a2f5abff595eabb4fa0e264a7aba8ff50
peeranat-dan <ppeeranat.d@gmail.com>
2023-05-16 10:44:30 +0700

    💄 change login form style

---
+++ src/containers/form-card-header/FormCardHeader.tsx
@@ -0,0 +1,21 @@
+import { ClockIcon } from '@heroicons/react/24/outline';
+import { useNavigate } from 'react-router-dom';
+
+import { CardHeader, CardTitle } from '@/components/card';
+
+const FormCardHeader = () => {
+  const navigate = useNavigate();
+  return (
+    <CardHeader className='bg-primary-100'>
+      <div
+        className='mx-auto flex w-fit cursor-pointer select-none items-center justify-center gap-1 text-center duration-100 hover:scale-105'
+        onClick={() => navigate('/')}
+      >
+        <CardTitle className='text-3xl font-medium text-primary-700'>Pomoration</CardTitle>
+        <ClockIcon className='h-10 w-10 text-primary-700' />
+      </div>
+    </CardHeader>
+  );
+};
+
+export default FormCardHeader;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,7 +2,8 @@ import { useEffect } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/card';
+import { Card, CardContent } from '@/components/card';
+import FormCardHeader from '@/containers/form-card-header';
 import CenteredLayout from '@/layouts/centered';
 import { useAuthContext } from '@/providers/auth/AuthProvider';
 
@@ -20,21 +21,19 @@ const Login = () => {
   return (
     <CenteredLayout>
       <div className='mx-auto w-full max-w-md space-y-4 px-4 sm:px-0 md:w-1/3 xl:w-1/4'>
-        <Card>
-          <CardHeader>
-            <CardTitle
-              className='cursor-pointer select-none text-center text-3xl font-semibold tracking-tight'
-              onClick={() => navigate('/')}
-            >
-              Pomoration
-            </CardTitle>
-          </CardHeader>
+        <Card className='space-y-4'>
+          <FormCardHeader />
           <CardContent className='space-y-4'>
             <LoginForm />
-            <div
-              onClick={() => navigate('/signup')}
-              className='cursor-pointer text-center text-primary duration-200 hover:underline'
-            >{`Don't have any account? Sign up here`}</div>
+            <div className='text-center'>
+              <p>
+                Don&apos;t have an account?
+                <a
+                  href='/signup'
+                  className='ml-1 cursor-pointer text-primary duration-200 hover:underline'
+                >{`Sign up`}</a>
+              </p>
+            </div>
           </CardContent>
         </Card>
       </div>
