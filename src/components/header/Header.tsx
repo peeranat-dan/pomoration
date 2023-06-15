a5f43f7e891b514032429e86679634c85e05248c
peeranat-dan <ppeeranat.d@gmail.com>
2023-06-15 15:55:26 +0700

    🎨 add button that navigate into setting route

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,6 @@
-import { Clock } from 'lucide-react';
+import { Clock, Settings } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
-import SettingButton from '@/containers/setting-button';
 import UserAvatarMenu from '@/containers/user-avatar-menu';
 import { useAuthContext } from '@/providers/auth/AuthProvider';
 
@@ -10,23 +9,36 @@ import { Button } from '../base';
 const Header = () => {
   const navigate = useNavigate();
   const { isLoggedIn } = useAuthContext();
+
+  const handleHome = () => {
+    navigate('/');
+  };
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  const handleSettings = () => {
+    navigate('/settings');
+  };
+
   return (
     <header className='header nav mx-auto flex max-w-xl items-center justify-between px-4 py-6 sm:px-0'>
       <div
         className='flex cursor-pointer select-none items-center gap-1 text-2xl font-semibold tracking-tight text-primary dark:text-primary'
-        onClick={() => navigate('/')}
+        onClick={handleHome}
       >
         Pomoration
         <Clock className='h-6 w-6 stroke-2' />
       </div>
       <div className='flex items-center gap-2'>
         {/* NOTE: In the future we'll redirect user to setting route */}
-        {isLoggedIn ? null : <SettingButton />}
-        {isLoggedIn ? (
-          <UserAvatarMenu />
-        ) : (
-          <Button onClick={() => navigate('/login')}>Login</Button>
+        {isLoggedIn ? null : (
+          <Button variant='ghost' onClick={handleSettings}>
+            <Settings className='h-6 w-6' />
+          </Button>
         )}
+        {isLoggedIn ? <UserAvatarMenu /> : <Button onClick={handleLogin}>Login</Button>}
       </div>
     </header>
   );
