b9343382ed947416041a06fd2e88b764df9617bb
peeranat-dan <ppeeranat.d@gmail.com>
2023-06-23 23:52:56 +0700

    🎨 use env from config

--- src/libs/supabase/initialize.ts
+++ src/libs/supabase/initialize.ts
@@ -1,10 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
+import { env } from '@/utils/config';
+
 import type { Database } from './database.types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_URL_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = env.supabase.url;
+const SUPABASE_ANON_KEY = env.supabase.anonKey;
 
-const client = createClient<Database>(SUPABASE_URL, SUPABASE_URL_ANON_KEY);
+const client = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export { client as supabaseClient };
