31e5d47162a3f6588ca5a33369325857b73c35a8
peeranat-dan <ppeeranat.d@gmail.com>
2023-06-23 23:52:08 +0700

    🔧 add github url in config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
     anonKey: z.string().default(''),
   }),
   appName: z.string().default('Pomoration'),
+  repositoryUrl: z.string().default(''),
 });
 
 export type Env = z.infer<typeof envSchema>;
@@ -16,4 +17,5 @@ export const env = envSchema.parse({
     anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
   },
   appName: import.meta.env.VITE_APP_NAME,
+  repositoryUrl: import.meta.env.VITE_GITHUB_REPOSITORY_URL,
 });
